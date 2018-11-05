44217fa0903b84439ff9bdc96ce5a43b140ea55f
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-11-05 14:29:56 -0600

    Add movement script, and ground detection script

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -2,15 +2,135 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterScript : MonoBehaviour {
+public class CharacterScript : MonoBehaviour
+{
+    //Rigid body variable. 
+    [SerializeField]
+    private Rigidbody2D myRigidBody;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField]
+    private float jumpForce = 10;
+
+    //Acceleration variable.
+    [SerializeField]
+    private float accelerationForce = 5;
+
+    //Max speed variable.
+    [SerializeField]
+    private float maxSpeed = 5;
+
+    [SerializeField]
+    private Collider2D playerGroundCollider;
+
+    [SerializeField]
+    private PhysicsMaterial2D playerMovingPhysicsMaterial, playerStoppingPhysicsMaterial;
+
+    [SerializeField]
+    private Collider2D groundDetectTrigger;
+
+    [SerializeField]
+    private ContactFilter2D groundContactFilter;
+
+    private Collider2D[] groundHitDetectionResults = new Collider2D[16];
+
+    private bool isOnGround;
+
+    private bool facingRight = true;
+
+    //Horizontal, and vertical input variables.
+    private float horizontalInput;
+
+
+    private void Update()
+    {
+        //Stops the rotation.
+        Vector3 currentRotation = transform.localEulerAngles;
+        currentRotation.z = 0;
+        transform.localEulerAngles = currentRotation;
+
+        //On Ground Update.
+        UpdateIsOnGround();
+
+        //Horizontal Movement Function.
+        UpdateHorizontalMovement();
+
+        //Jump Input Function.
+        HandleJumpInput();
+    }
+
+    //Fixed update for movement.
+    void FixedUpdate()
+    {
+        UpdatePhysicsMaterial();
+
+        //Calls Move function.
+        Move();
+
+        if (horizontalInput > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (horizontalInput < 0 && facingRight)
+        {
+            Flip();
+        }
+
+    }
+
+    private void UpdatePhysicsMaterial()
+    {
+        if (Mathf.Abs(horizontalInput) > 0)
+        {
+            playerGroundCollider.sharedMaterial = playerMovingPhysicsMaterial;
+        }
+
+        else
+        {
+            playerGroundCollider.sharedMaterial = playerStoppingPhysicsMaterial;
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    private void UpdateIsOnGround()
+    {
+        isOnGround = groundDetectTrigger.OverlapCollider(groundContactFilter, groundHitDetectionResults) > 0;
+
+        //Debug.Log("Is On Ground?: " + isOnGround);
+    }
+
+
+    private void UpdateHorizontalMovement()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+    }
+
+    //Movement function.
+    private void Move()
+    {
+        myRigidBody.AddForce(Vector2.right * horizontalInput * accelerationForce);
+
+        Vector2 clampedVelocity = myRigidBody.velocity;
+
+        clampedVelocity.x = Mathf.Clamp(myRigidBody.velocity.x, -maxSpeed, maxSpeed);
+
+        myRigidBody.velocity = clampedVelocity;
+
+    }
+
+
+    //Jump function.
+    private void HandleJumpInput()
+    {
+        if (Input.GetButtonDown("Jump") && isOnGround)
+        {
+            myRigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
 }
