e12d0d4745855685464be168fd042dfae309e83f
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-11-18 18:22:26 -0600

    Fixed sorting layer, and added spike script detection

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spike : MonoBehaviour {
+public class Spike : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            CharacterScript player = collision.GetComponent<CharacterScript>();
+
+            Debug.Log("Player entered the trigger.");
+
+           
+        }
+    }
 }
