c37596cb468323ded9074e0f2699cd3a30080562
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-11-18 19:56:43 -0600

    Fixed Spike script

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -9,7 +9,7 @@ public class Spike : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
 
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.GetType() == typeof(BoxCollider2D))
         {
             CharacterScript player = collision.GetComponent<CharacterScript>();
 
