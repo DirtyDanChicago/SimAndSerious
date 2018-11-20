05ea797f7a55e5ff578241d6d08a90f3e1250039
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-11-19 19:03:34 -0600

    Fixed some random shiz

--- Assets/Scripts/FollowScriptt.cs
+++ Assets/Scripts/FollowScriptt.cs
@@ -87,7 +87,7 @@ public class FollowScriptt : MonoBehaviour
     {
         Vector2 position = transform.position;
         Vector2 direction = Vector2.down;
-        float distance = 6.0f;
+        float distance = 10.0f;
         Debug.DrawRay(position, direction, Color.green);
         RaycastHit2D hit = Physics2D.Raycast(position, direction, distance, groundLayer);
         if (hit.collider != null)
