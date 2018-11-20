241364cd83edfcd099fb9b11d80173a8448c6a2f
DirtyDanChicago <32584314+DirtyDanChicago@users.noreply.github.com>
2018-11-19 18:22:21 -0600

    Fixed spikes and shit

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -19,14 +19,4 @@ public class Spike : MonoBehaviour {
         }
     }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 }
