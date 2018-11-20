f2d38fa52544655e8255f4d0a3a19a3fa0c4d440
Luke Uritescu <Luritescu@gmail.com>
2018-11-19 20:26:44 -0600

    fix scene loading

--- Assets/Scripts/PodiumScript.cs
+++ Assets/Scripts/PodiumScript.cs
@@ -6,12 +6,12 @@ using UnityEngine.SceneManagement;
 public class PodiumScript : MonoBehaviour
 {
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             SceneManager.LoadScene("Ending");
         }
     }
-	
+
 }
